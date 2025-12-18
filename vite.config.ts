34d8436bdf5854883b67b0e3a564f1d0a20bffd0
Heliacer <heliacer@gmx.ch>
2025-12-18 12:38:19 +0100

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/graplet-v3',
+  base: '/',
   build: {
     rollupOptions: {
       input: {
